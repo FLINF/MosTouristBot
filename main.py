267009cebe5a4f4171ffdd58fc5b432573a8c935
FLINF <54003914+FLINF@users.noreply.github.com>
2021-05-06 13:44:19 +0300

    /findPark now works as intended

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ from variables import *
 
 bot = telebot.TeleBot(TOKEN)
 
-#db = sqlite3.connect("MosTourist.db")
-#cur = db.cursor()
-
-
 @bot.message_handler(commands=['start'])  # Команда для начала работы
 def start_handler(message):
-    bot.send_message(message.from_user.id, startMessage)
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button_help = types.KeyboardButton(text='/help')
+    button_find_park = types.KeyboardButton(text='/findPark')
+    button_find_sight = types.KeyboardButton(text='/findSight')
+    button_weather = types.KeyboardButton(text='/weather')
+    keyboard.add(button_help, button_find_park, button_find_sight, button_weather)
+    bot.send_message(message.from_user.id, startMessage, reply_markup=keyboard)
 
 
 @bot.message_handler(commands=['help'])  # Список команд
@@ -33,23 +35,13 @@ def weather_handler(message):
 @bot.message_handler(commands=['findPark'])  # Найти ближайший парк
 def find_park_handler(message):
     keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    button_geo = types.KeyboardButton(text="Отрпавить геолокацию", request_location=True)
+    button_geo = types.KeyboardButton(sendLocationButtonText, request_location=True)
     keyboard.add(button_geo)
     bot.send_message(message.from_user.id, sendLocationMessage, reply_markup=keyboard)
 
-
-@bot.message_handler(content_types=['location'])
-def location_handler(location):
-    db = sqlite3.connect("MosTourist.db")
-    print("debug......................................")
-    coordinates = (location.location.latitude, location.location.longitude)
-    print(coordinates)
-  # result = findParkCommand.find_park(coordinates)  # На основе полученных координат find_park выполняет SQL запрос
-    cur1 = db.cursor()
-    cur1.execute(f"""SELECT * FROM park ORDER BY ABS(longitude - {coordinates[0]} + latitude - {coordinates[1]})""")
-    result = cur1.fetchall()
-    bot.send_message(location.from_user.id, result)
-
+    @bot.message_handler(content_types=['location'])
+    def find_park(message):
+        findParkCommand.location_handler(message)
 
 @bot.message_handler(commands=['findSight'])  # Поиск достопримечательности
 def find_sight_handler(message):
