79e755a0f1f5226e9fba51ba7f30b32d8bc53b54
FLINF <54003914+FLINF@users.noreply.github.com>
2021-05-23 16:37:45 +0300

    .

--- findSightCommand.py
+++ findSightCommand.py
@@ -3,7 +3,7 @@ import sqlite3
 
 from selenium import webdriver
 from fuzzywuzzy import fuzz
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 from main import bot, standart_markup
 from PIL import Image
 from io import BytesIO
@@ -42,7 +42,7 @@ def get_sight_by_photo(message):
     im.save(os.getcwd()+fr"/temp/photo{message.from_user.id}.jpg")  # Сохжраняем фото в папку temp
     edge = webdriver.Edge()  # Открываем браузер
     edge.get("https://yandex.ru/images/")
-    WebDriverWait(edge, 30).until(ec.element_to_be_clickable((By.XPATH, ".//*[@class='button2 button2_theme_clear "
+    WebDriverWait(edge, 25).until(ec.element_to_be_clickable((By.XPATH, ".//*[@class='button2 button2_theme_clear "
                                                                         "button2_size_m button2_view_classic"
                                                                         " i-bem']"))).click()
     element = edge.find_element_by_xpath('.//input[@class="cbir-panel__file-input"]')
@@ -59,10 +59,14 @@ def get_sight_by_photo(message):
         for row in sight_list:  # Сравнение наденного в Яндексе с данными из БД
             match = fuzz.token_sort_ratio(name, row[1])
             match_percent.append(match)
-        result = sight_list[match_percent.index(max(match_percent))]
+        if max(match_percent) > 10:  # Процент сходства должен быть больше 0
+            result = sight_list[match_percent.index(max(match_percent))]
+            print(max(match_percent))
+        else:
+            result = 'К сожалению, нам не удалось найти достопимечательность на фотографии.'
 
-    except NoSuchElementException:  # Объекта не найден
-        print('Element noе found exception')
+    except TimeoutException:  # Объекта не найден
+        print('Timeout exception.')
         bot.send_message(message.from_user.id, 'К сожалению, нам не удалось найти достопимечательность на фотографии.',
                          reply_markup=standart_markup())
         edge.quit()
